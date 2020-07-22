40875fd6909f4cd4e24485d5edb7a82e0eb6e8a5
Mark Verbeek <markverbeek@mac.com>
2020-07-22 16:13:07 +0200

    added /scream (create) route

--- functions/index.js
+++ functions/index.js
@@ -22,10 +22,7 @@ app.get("/screams", (req, res) => {
 })
 
 
-exports.createScream = functions.https.onRequest((req, res) => {
-    if (req.method !== 'POST') {
-        return res.status(400).json({ error: "Method is not allowed" });
-    }
+app.post("/scream", (req, res) => {
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
