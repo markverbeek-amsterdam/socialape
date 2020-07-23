b4f525d8f4cf729f269e90976a3eea915f330172
Mark Verbeek <markverbeek@mac.com>
2020-07-23 22:02:38 +0200

    get comments route

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ const app = require('express')();
 
 const FBAuth = require('./util/fbAuth');
 
-const { getAllScreams, postOneScream } = require('./handlers/screams');
+const { getAllScreams, postOneScream, getScream } = require('./handlers/screams');
 const {
     signup,
     login,
@@ -16,6 +16,11 @@ const {
 // Scream routes
 app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
+//TODO: delete scream
+//TODO: like scream
+//TODO: unlike scream
+//TODO: comment on scream
 
 
 
