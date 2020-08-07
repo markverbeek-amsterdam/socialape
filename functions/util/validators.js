300cb603b553b99197aad840f37fbcdd9c1832ca
Mark Verbeek <markverbeek@mac.com>
2020-08-07 12:20:05 +0200

    fixed login validation error

--- functions/util/validators.js
+++ functions/util/validators.js
@@ -7,6 +7,7 @@ const isEmail = (email) => {
 }
 
 const isEmpty = (string) => {
+    console.log('string from isEmpty' + string)
     if (string.trim() === '') return true;
     else return false;
 };
@@ -36,8 +37,6 @@ exports.validateLoginData = (data) => {
 
     if (isEmpty(data.email)) errors.email = "Must not be empty";
     if (isEmpty(data.password)) errors.password = "Must not be empty";
-
-    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
     return {
         errors,
